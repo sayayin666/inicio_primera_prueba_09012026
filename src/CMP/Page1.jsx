b23ef897fc8692d0c1fcb5c87047f3ba62570b50
Sotelo Joan Sebastián <sayayinjohan4@gmail.com>
2026-01-11 17:22:37 -0500

    MNQINTENSIVO

--- src/CMP/Page1.jsx
+++ src/CMP/Page1.jsx
@@ -1,11 +1,15 @@
-import { Link } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 function Page1() {
 
+    
+    function saltarPagina() {
+        
+    }
     return(
         <div>
-            <img src="https://thefappeningblog.com/content/01/01/marleybrinx/marleybrinx_nude_leaks_thefappeningblog.com_000.jpg"></img>
-            <h1>MARLEY_BRINX</h1>
-            <button /*onClick={saltarPagina}*/>IR PAGINA_2</button>
+            <img src="https://uploads.builtforbackroads.com/uploads/2023/05/2023.05.21-NISSAN-Z33-350Z-NISMO-2008_1-1536x1024.jpg"></img>
+            <h1>NISSAN_350Z</h1>
+            <button onClick={saltarPagina}>IR PAGINA_2</button>
         </div>
     )
 }
--- src/CMP/Page2.jsx
+++ src/CMP/Page2.jsx
@@ -1,8 +1,8 @@
 function Page2() {
     return(
         <div>
-            <img src="https://pic.r18hub.com/pornpicsi/1280/7/500/26532789/26532789_001_a90b.jpg"></img>
-            <h1>KORTNEY_KANE</h1>
+            <img src="https://cdn.wallpapersafari.com/58/71/HN2jxP.jpg"></img>
+            <h1>MITSUBISHI_LANCER_EVO_9</h1>
             <button /*onClick={irapagina1}*/></button>
         </div>
     )
