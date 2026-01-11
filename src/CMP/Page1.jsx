68379da9265476292bdae7a216537ac6ec279600
Sotelo Joan Sebastián <sayayinjohan4@gmail.com>
2026-01-11 16:33:03 -0500

    install r.r.d. & act pg1 y pg2

--- src/CMP/Page1.jsx
+++ src/CMP/Page1.jsx
@@ -1,9 +1,10 @@
-import {  } from 'react'
+import { Link } from 'react-router-dom'
 function Page1() {
 
     return(
         <div>
             <img src="https://thefappeningblog.com/content/01/01/marleybrinx/marleybrinx_nude_leaks_thefappeningblog.com_000.jpg"></img>
+            <h1>MARLEY_BRINX</h1>
             <button /*onClick={saltarPagina}*/>IR PAGINA_2</button>
         </div>
     )
--- src/CMP/Page2.jsx
+++ src/CMP/Page2.jsx
@@ -2,6 +2,7 @@ function Page2() {
     return(
         <div>
             <img src="https://pic.r18hub.com/pornpicsi/1280/7/500/26532789/26532789_001_a90b.jpg"></img>
+            <h1>KORTNEY_KANE</h1>
             <button /*onClick={irapagina1}*/></button>
         </div>
     )
